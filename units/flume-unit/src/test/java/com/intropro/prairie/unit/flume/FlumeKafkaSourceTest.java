d6e16d3c216c586c557492d79217523803a3ac8d
Vitalii Gergel <vgergel@intropro.com>
2015-11-05 22:08:31 +0200

    flume kafka source test

--- units/flume-unit/src/test/java/com/intropro/prairie/unit/flume/FlumeKafkaSourceTest.java
+++ units/flume-unit/src/test/java/com/intropro/prairie/unit/flume/FlumeKafkaSourceTest.java
@@ -3,6 +3,8 @@ package com.intropro.prairie.unit.flume;
 import com.intropro.prairie.comparator.ByLineComparator;
 import com.intropro.prairie.junit.BigDataTestRunner;
 import com.intropro.prairie.unit.common.annotation.BigDataUnit;
+import com.intropro.prairie.unit.kafka.KafkaProducer;
+import com.intropro.prairie.unit.kafka.KafkaUnit;
 import com.intropro.prairie.utils.EventChecker;
 import com.intropro.prairie.utils.FileUtils;
 import com.intropro.prairie.utils.Waiter;
@@ -13,42 +15,52 @@ import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 /**
- * Created by presidentio on 10/14/15.
+ * Created by presidentio on 11/4/15.
  */
 @RunWith(BigDataTestRunner.class)
-public class FlumeUnitTest {
+public class FlumeKafkaSourceTest {
 
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
 
     @BigDataUnit
     private FlumeUnit flumeUnit;
-
+    @BigDataUnit
+    private KafkaUnit kafkaUnit;
     private ByLineComparator<String> byLineComparator = new ByLineComparator<>();
 
+    private String topic = "test_topic";
+    private int messageCount = 10;
+
     @Test
-    public void testFlumeUnit() throws Exception {
+    public void testFlumeUnitKafkaSource() throws Exception {
         Properties properties = new Properties();
-        properties.load(FlumeUnitTest.class.getClassLoader().getResourceAsStream("custom-source/flume.properties"));
-        final String[] events = properties.getProperty("test_agent.sources.source.items").split(",");
+        properties.load(FlumeUnitTest.class.getClassLoader().getResourceAsStream("kafka-source/flume.properties"));
+        properties.setProperty("test_agent.sources.kafka-source.zookeeperConnect", kafkaUnit.getZookeeperConnect());
+        properties.setProperty("test_agent.sources.kafka-source.topic", topic);
         File outputFolder = folder.newFolder("test_agent");
         properties.setProperty("test_agent.sinks.sink.sink.directory", outputFolder.getAbsolutePath());
         final FlumeAgent flumeAgent = flumeUnit.createAgent("test_agent", properties);
         flumeAgent.start();
+        KafkaProducer kafkaProducer = kafkaUnit.createProducer(topic);
+        final List<String> messages = new ArrayList<>();
+        for (int i = 0; i < messageCount; i++) {
+            String message = UUID.randomUUID().toString();
+            messages.add(message);
+            kafkaProducer.put(message);
+        }
         new Waiter(5000, new EventChecker() {
             @Override
             public boolean check() {
-                return flumeAgent.getSink("sink").processedEventCount() == events.length;
+                return flumeAgent.getSink("sink").processedEventCount() == messages.size();
             }
         }).await();
-        Assert.assertEquals("Unexpected event count processed", events.length, flumeAgent.getSink("sink").processedEventCount());
+        Assert.assertEquals("Unexpected event count processed", messages.size(), flumeAgent.getSink("sink").processedEventCount());
         flumeAgent.close();
         List<String> resultLines = FileUtils.readLineInDirectory(outputFolder);
-        byLineComparator.compare(Arrays.asList(events), resultLines).assertEquals();
+        byLineComparator.compare(messages, resultLines).assertEquals();
     }
 }
