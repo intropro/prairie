c430c7f4d235cfa3c1ac638be109aa161c4e1ba5
Vitalii Gergel <president603@gmail.com>
2015-11-18 21:34:32 +0200

    support lower hive version, resolve #1

--- units/hive2-unit/src/main/java/com/intropro/prairie/unit/hive2/Hive2Unit.java
+++ units/hive2-unit/src/main/java/com/intropro/prairie/unit/hive2/Hive2Unit.java
@@ -2,9 +2,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -86,11 +86,14 @@ public class Hive2Unit extends HadoopUnit {
         hiveConf.setBoolVar(HiveConf.ConfVars.HIVECONVERTJOIN, false);
         hiveConf.setBoolVar(HiveConf.ConfVars.HIVESKEWJOIN, false);
         hiveConf.setBoolVar(HiveConf.ConfVars.LOCALMODEAUTO, false);
-        hiveConf.setBoolVar(HiveConf.ConfVars.SUBMITLOCALTASKVIACHILD, false);
+        try {
+            hiveConf.setBoolVar(HiveConf.ConfVars.SUBMITLOCALTASKVIACHILD, false);
+        } catch (NoSuchFieldError e) {
+            LOGGER.warn("Can't disable SUBMITLOCALTASKVIACHILD, HiveConf.ConfVars does not contains such field");
+        }
         hiveConf.setVar(HiveConf.ConfVars.HIVE_SERVER2_THRIFT_BIND_HOST, "localhost");
         String metaStorageUrl = "jdbc:hsqldb:mem:" + UUID.randomUUID().toString() + ";create=true";
         hiveConf.setVar(HiveConf.ConfVars.METASTORECONNECTURLKEY, metaStorageUrl);
-//        hiveConf.setVar(HiveConf.ConfVars.HIVE_SERVER2_TRANSPORT_MODE, "http");
         hiveServer = new HiveServer2();
         hiveServer.init(hiveConf);
         hiveServer.start();
